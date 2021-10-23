4fcf49dc287e85b32a80068802d3fa37ba438b56
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-10-23 13:27:14 +0200

    working with tables - adding bootstrap for styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface CoinsInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24: number;
+  total_volume: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +18,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
+
+  coins: CoinsInterface[] = [];
+
+
   constructor (private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    this.http.get<CoinsInterface[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .subscribe(
     (response) => {
       console.log(response)
+      this.coins = response
     },
     (error) => console.log(error)
     );
