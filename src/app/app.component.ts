9fffd736161018fe12702ea1bf5c722f97020473
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-10-23 12:24:23 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'crypto-pricing';
+export class AppComponent implements OnInit {
+
+  constructor (private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .subscribe(
+    (response) => {
+      console.log(response)
+    },
+    (error) => console.log(error)
+    );
+
+
+}
 }
